d399e341afae7426583c17516a14c432d766ade5
Justin Turpin <ionstream@gmail.com>
2018-11-04 07:30:18 -0800

    optional query, move to root path

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ use serde_derive::{Serialize, Deserialize};
 
 #[derive(Deserialize, StateData, StaticResponseExtender)]
 struct QueryStringExtractor {
-    query: String,
+    query: Option<String>,
 }
 
 #[derive(Debug, Serialize)]
@@ -50,7 +50,7 @@ lazy_static! {
 
 fn router() -> Router {
     build_simple_router(|route| {
-        route.get("/search")
+        route.get("/")
             .with_query_string_extractor::<QueryStringExtractor>()
             .to(search);
     })
@@ -59,28 +59,33 @@ fn router() -> Router {
 pub fn search(mut state: State) -> (State, Response<Body>) {
     let conn = rusqlite::Connection::open("database.sqlite3").unwrap();
 
-    let query_param = QueryStringExtractor::take_from(&mut state);
-
-    let mut stmt = conn
-        .prepare("SELECT name, version, description FROM packages WHERE name MATCH ?1 LIMIT 50")
-        .unwrap();
-
-    let package_names : Vec<Package> = stmt.query_map(
-            &[&query_param.query],
-            |row| Package{
-                name: row.get(0),
-                version: row.get(1),
-                description: row.get(2)
-            }
-        )
-        .unwrap()
-        .map(|element| element.unwrap())
-        .collect();
-
     let mut template_context = tera::Context::new();
 
-    template_context.insert("results", &package_names);
-    template_context.insert("query", &query_param.query);
+    let query_param = QueryStringExtractor::take_from(&mut state);
+
+    if let Some(query_string) = query_param.query {
+        let mut stmt = conn
+            .prepare("SELECT name, version, description FROM packages WHERE name MATCH ?1 LIMIT 50")
+            .unwrap();
+
+        let package_names : Vec<Package> = stmt.query_map(
+                &[&query_string],
+                |row| Package{
+                    name: row.get(0),
+                    version: row.get(1),
+                    description: row.get(2)
+                }
+            )
+            .unwrap()
+            .map(|element| element.unwrap())
+            .collect();
+
+
+        template_context.insert("results", &package_names);
+        template_context.insert("query", &query_string);
+    } else {
+        template_context.insert("query", &"".to_string());
+    }
 
     let contents = TERA.render("index.html", &template_context).unwrap();
 
@@ -90,19 +95,6 @@ pub fn search(mut state: State) -> (State, Response<Body>) {
 
     (state, res)
 }
-
-
-// pub fn say_hello(state: State) -> Box<HandlerFuture> {
-//     let request = request_async(String::from("http://httpbin.org"));
-
-//     Box::new(request.then(|result| {
-//         let res = create_response(
-//             &state, StatusCode::Ok, Some((result.unwrap(), mime::TEXT_PLAIN))
-//         );
-
-//         Ok((state, res))
-//     }))
-// }
 
 pub fn main() {
     let addr = "0.0.0.0:8080";
