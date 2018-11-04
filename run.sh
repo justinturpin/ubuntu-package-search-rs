52294a844a2f4c0fabffaf48c5d5f1ed7be8b65a
Justin Turpin <ionstream@gmail.com>
2018-11-03 20:38:31 -0700

    absolute name, utf fixes

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
 python3 load_data.py load-latest-packages
 python3 load_data.py load-latest-contents
 
-exec ubuntu-package-search
+exec /opt/ubuntu-package-search
